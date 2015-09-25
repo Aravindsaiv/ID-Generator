becb89972815cafdb525cf7f295fe9762db72841
aravindsaiv <aravindappu2@gmail.com>
2015-09-25 21:38:47 +0530

    Made Alterations. - Completed the base HTML. - Added styles.

--- app.py
+++ app.py
@@ -2,9 +2,11 @@ from flask import Flask,request,render_template,flash
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', method=['GET','POST'])
 def index():
 	return render_template(index.html)
+	if method.type == 'POST':
+		
 
 if __name__ == '__main__ ':
 	app.run(debug=True)
